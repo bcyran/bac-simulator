4635e3c6209322012b1311583a611015956ae3c0
Bazyli Cyran <bazyli.cyran@gmail.com>
2016-02-15 17:47:03 +0100

    User preferences are now global

--- bac-simulator/bac-simulator.py
+++ bac-simulator/bac-simulator.py
@@ -4,6 +4,10 @@ from pprint import pprint
 from math import exp
 
 
+# User preferences
+USER_PREFS = {}
+
+
 # Read alcohol intakes data from file
 def read_data(data_file):
     # Read raw lines form file
@@ -26,19 +30,18 @@ def read_data(data_file):
 
 # Read user preferences (first line of data file)
 def read_prefs(data_file):
+    global USER_PREFS
+
     with open(data_file) as f:
         raw_prefs = f.readline()
 
     raw_prefs = raw_prefs.split()
 
-    user_prefs = {}
-    user_prefs['sex'] = raw_prefs[0]
-    user_prefs['weight'] = float(raw_prefs[1])
-    user_prefs['height'] = float(raw_prefs[2])
-    user_prefs['absorption_rate'] = float(raw_prefs[3])
-    user_prefs['interval'] = float(raw_prefs[4])
-
-    return user_prefs
+    USER_PREFS['sex'] = raw_prefs[0]
+    USER_PREFS['weight'] = float(raw_prefs[1])
+    USER_PREFS['height'] = float(raw_prefs[2])
+    USER_PREFS['abs_rate'] = float(raw_prefs[3])
+    USER_PREFS['interval'] = float(raw_prefs[4])
 
 
 # Calculate alcohol elimination rate (AER)
@@ -100,9 +103,9 @@ def calc_bac(absorbed, weight, vda, eliminated):
 
 # Create absorption table
 # (alcohol absorbed for each intake at different timestamps)
-def calc_abs_tab(intakes_tab, interval, absorption_rate):
+def calc_abs_tab(intakes_tab):
     start_time = intakes_tab[0][0]
-    intakes_count = len(intakes_tab)
+    i_count = len(intakes_tab)
     abs_tab = []
 
     current_time = start_time
@@ -113,7 +116,7 @@ def calc_abs_tab(intakes_tab, interval, absorption_rate):
         abs_tab.append([])
 
         # Iterate through columns (intakes)
-        for j in range(0, intakes_count):
+        for j in range(0, i_count):
             # If this intake didn't yet happen ignore it
             if current_time < intakes_tab[j][0]:
                 abs_tab[i].append(0)
@@ -122,16 +125,16 @@ def calc_abs_tab(intakes_tab, interval, absorption_rate):
             # Calculate and write absorbed amount
             amount = intakes_tab[j][1]
             delta = (current_time - intakes_tab[j][0]).seconds / 3600
-            absorbed = calc_absorbed(amount, absorption_rate, delta)
+            absorbed = calc_absorbed(amount, USER_PREFS['abs_rate'], delta)
             abs_tab[i].append(absorbed)
 
         # Stop loop when last intake is fully absorbed
-        if (abs_tab[i][intakes_count - 1] == intakes_tab[intakes_count - 1][1] and
-                intakes_tab[intakes_count - 1][0] < current_time):
+        if (abs_tab[i][i_count - 1] == intakes_tab[i_count - 1][1] and
+                intakes_tab[i_count - 1][0] < current_time):
             break
 
         # Add set interval to current time
-        current_time = current_time + timedelta(minutes=interval)
+        current_time = current_time + timedelta(minutes=USER_PREFS['interval'])
 
         i += 1
 
@@ -140,7 +143,7 @@ def calc_abs_tab(intakes_tab, interval, absorption_rate):
 
 # Table with total absorption, total elimination
 # and current bac for each timestamp
-def calc_bac_tab(abs_tab, interval, sex, weight, vda):
+def calc_bac_tab(abs_tab, vda):
     bac_tab = []
 
     i = 0
@@ -158,14 +161,15 @@ def calc_bac_tab(abs_tab, interval, sex, weight, vda):
         if i > 0:
             prev_bac = bac_tab[i - 1][2]
             total_eliminated = bac_tab[i - 1][1]
-            current_aer = calc_aer(sex, prev_bac)
-            current_eliminated = current_aer * (interval / 60)
+            current_aer = calc_aer(USER_PREFS['sex'], prev_bac)
+            current_eliminated = current_aer * (USER_PREFS['interval'] / 60)
             total_eliminated += current_eliminated
         else:
             total_eliminated = 0
 
         # Calculate current BAC
-        current_bac = calc_bac(total_absorbed, weight, vda, total_eliminated)
+        current_bac = calc_bac(total_absorbed, USER_PREFS['weight'],
+                               vda, total_eliminated)
 
         if current_bac < 0.01:
             current_bac = 0
@@ -183,7 +187,7 @@ def calc_bac_tab(abs_tab, interval, sex, weight, vda):
 
 
 # Print out calculated data
-def print_data(bac_tab, start_time, interval):
+def print_data(bac_tab, start_time):
     current_time = start_time
     print('TIME  - BAC')
 
@@ -191,7 +195,7 @@ def print_data(bac_tab, start_time, interval):
         time = current_time.strftime('%H:%M')
         print('{0} - {1:.2f}'.format(time, row[2]))
 
-        current_time = current_time + timedelta(minutes=interval)
+        current_time = current_time + timedelta(minutes=USER_PREFS['interval'])
 
 
 # Main
@@ -199,28 +203,20 @@ def main():
     # Name of file containing all required data
     data_file = 'bac-simulator-data.txt'
 
+    # Read user preferences
+    read_prefs(data_file)
+
     # Read data from file
     intakes_tab = read_data(data_file)
-    user_prefs = read_prefs(data_file)
 
     # Calculate everything
-    abs_tab = calc_abs_tab(
-        intakes_tab,
-        user_prefs['interval'],
-        user_prefs['absorption_rate'])
-    vda = calc_vda(
-        user_prefs['sex'],
-        user_prefs['weight'],
-        user_prefs['height'])
-    bac_tab = calc_bac_tab(
-        abs_tab,
-        user_prefs['interval'],
-        user_prefs['sex'],
-        user_prefs['weight'],
-        vda)
+    vda = calc_vda(USER_PREFS['sex'], USER_PREFS['weight'],
+                   USER_PREFS['height'])
+    abs_tab = calc_abs_tab(intakes_tab)
+    bac_tab = calc_bac_tab(abs_tab, vda)
 
     # And print result
-    print_data(bac_tab, intakes_tab[0][0], user_prefs['interval'])
+    print_data(bac_tab, intakes_tab[0][0])
 
 
 if __name__ == '__main__':
